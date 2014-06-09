ffec01c8c79e5660e42c38d937280648f181e5db
Wes Contreras <wes@wescontreras.com>
2014-06-09 11:11:21 -0700

    Correcting typo

--- routes/roll.js
+++ routes/roll.js
@@ -2,7 +2,7 @@ roll = function(req, res) {
   dice = require('../lib/parsedice')(req);
   dice.explain = require('../lib/explaindice')(dice);
   dice = require('../lib/calcdice')(dice);
-  require('../lib/sendOutput')(dice, res);
+  require('../lib/sendoutput')(dice, res);
 }
 
 module.exports = roll;
